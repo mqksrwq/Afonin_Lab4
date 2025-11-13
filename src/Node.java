c01208b6ff16e0f6950337b47fb36efdfd300981
mqksrwq <maksim4ikb2006@gmail.com>
2025-11-13 21:58:27 +0300

    delete node class

--- src/Node.java
+++
@@ -1,9 +0,0 @@
-public class Node<Double> {
-    public double value;
-    public Node<Double> next;
-
-    public Node(double value) {
-        this.value = value;
-        this.next = null;
-    }
-}
